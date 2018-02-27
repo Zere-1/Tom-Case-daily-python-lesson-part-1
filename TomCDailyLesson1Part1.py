dd92a9793160d542078bc4a8877c0154833feda2
Zere-1 <34482384+Zere-1@users.noreply.github.com>
2018-02-27 08:50:21 -0700

    Add files via upload

---
+++ TomCDailyLesson1Part1.py
@@ -0,0 +1,6 @@
+#Tom C Python daily lesson 1 part 1 2/27/18
+
+name = input("Enter your name: ")
+name = name[0].upper() + name[1:].lower()
+
+print("Hello " + name)
---
+++ lTomCLesson1Part2.py
@@ -0,0 +1,6 @@
+#Tom C Python daily lesson 1 2/27/18
+
+z = input("Enter your name: ")
+x = input("Enter your age: ")
+newNumber = int(x) + 100
+print (str(z) + " you will be " + str(newNumber) + " years old")
